516dc42668709754057c216fbf31088ad97e086f
LeticiaIruela <leticia.miruela@gmail.com>
2023-04-17 20:12:37 +0200

    Leti_bonus

---
+++ file.py
@@ -0,0 +1,14 @@
+from vikingsClasses import*
+def teams():
+    vikings=[Viking("Bego",50,60)],Viking("Pablo",50,60),Viking("Miguel",60,80)
+    saxons=[Saxon(50,80),Saxon(80,80),Saxon(70,30)]
+    wargame=()
+    for viking in vikings:
+        war.addViking(viking)
+    for saxon in saxons:
+        war.addSaxon(saxon)
+    return wargame
+
+
+
+
